79ed423c94f6acbd3a4658fd20bb0996c9b5f158
Rupsha Das <drupsha81@gmail.com>
2026-06-25 23:32:38 +0530

    Refactor nested loops for number pyramid generation

--- Number Pattern/NumberPyramid.java
+++ Number Pattern/NumberPyramid.java
@@ -3,8 +3,8 @@ public class NumberPyramid{
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
         int n=sc.nextInt();
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=n-i;j++){
+        for(int i=1;i<=n;i++){ //Row count
+            for(int j=1;j<=n-i;j++){ //Column count
                 System.out.print(" ");
             }
             for(int j=1;j<=i;j++){
