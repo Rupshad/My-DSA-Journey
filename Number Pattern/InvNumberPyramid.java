9fec2e514f2f030264381c2ec3e802bfb6b52856
Rupsha Das <drupsha81@gmail.com>
2026-07-04 21:19:37 +0530

    Refactor InvNumberPyramid for better readability
    Refactor code formatting and improve readability.

--- Number Pattern/InvNumberPyramid.java
+++ Number Pattern/InvNumberPyramid.java
@@ -1,20 +1,26 @@
 import java.util.Scanner;
+
 public class InvNumberPyramid {
     public static void main(String[] args) {
-        Scanner sc= new Scanner(System.in);
-        int n= sc.nextInt();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
 
-        for(int i=n;i>=1;i--){  //Row count
-            for(int j=1;j<=n-i;j++){  //Column count
+        // Rows
+        for (int i = n; i >= 1; i--) {
+
+            // Print spaces
+            for (int j = 1; j <= n - i; j++) {
                 System.out.print(" ");
             }
-            for(int j=1;j<=i;j++){
-                System.out.print(i+" ");
-            }
-            System.out.println();
 
+            // Print numbers
+            for (int j = 1; j <= i; j++) {
+                System.out.print(i + " ");
+            }
+
+            System.out.println();
         }
+
         sc.close();
-    
-}
+    }
 }
