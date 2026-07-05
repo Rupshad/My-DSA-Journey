e4035bc23d399d6496337595442b92baba89cb75
Rupsha Das <drupsha81@gmail.com>
2026-07-05 23:48:39 +0530

    Improve Insertion Sort method and main function
    Refactor Insertion Sort implementation for clarity and comments.

--- Sorting/InsertionSort.java
+++ Sorting/InsertionSort.java
@@ -1,28 +1,44 @@
 import java.util.Arrays;
 
 public class InsertionSort {
-    public static int[] Insertion(int[] arr){
-        for(int i=1;i<arr.length;i++){
-            int j=i;
-            
-            
-                while(j>0 && arr[j]<arr[j-1]){
-                    int temp=arr[j];
-                    arr[j]=arr[j-1];
-                    arr[j-1]=temp;
-                    j--;
-                }
-            
+
+    // Function to sort the array using Insertion Sort
+    public static int[] Insertion(int[] arr) {
+
+        // Start from the second element since the first element
+        // is considered sorted
+        for (int i = 1; i < arr.length; i++) {
+
+            // j points to the current element to be inserted
+            int j = i;
+
+            // Move the current element to its correct position
+            // by swapping it with larger elements on its left
+            while (j > 0 && arr[j] < arr[j - 1]) {
+
+                // Swap adjacent elements
+                int temp = arr[j];
+                arr[j] = arr[j - 1];
+                arr[j - 1] = temp;
+
+                // Move one position to the left
+                j--;
+            }
         }
 
-        
+        // Return the sorted array
         return arr;
     }
-    public static void main(String[] args){
-        int arr[]={5,4,3,2,1};
-        int arr1[]=Insertion(arr);
-        System.out.println("the sorted array is :"+ Arrays.toString(arr1));
 
+    public static void main(String[] args) {
 
+        // Input array
+        int[] arr = {5, 4, 3, 2, 1};
+
+        // Sort the array
+        int[] arr1 = Insertion(arr);
+
+        // Print the sorted array
+        System.out.println("The sorted array is: " + Arrays.toString(arr1));
     }
 }
