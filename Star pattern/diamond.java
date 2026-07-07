40acee2246a80eaf058ccdfe31a56af2e769d9ff
Rupsha Das <drupsha81@gmail.com>
2026-07-07 23:16:11 +0530

    Refactor diamond pattern implementation
    Refactor diamond class to improve readability and structure.

--- Star pattern/diamond.java
+++ Star pattern/diamond.java
@@ -1,31 +1,47 @@
 import java.util.Scanner;
 
-public class diamond {
+public class Diamond {
+
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
+
+        // Read the number of rows for the upper half
         int n = sc.nextInt();
+
+        // Print the upper half of the diamond
         for (int i = 1; i <= n; i++) {
+
+            // Print leading spaces
             for (int j = 1; j <= n - i; j++) {
                 System.out.print(" ");
             }
-            for (int k = 1; k <= 2 * i - 1; k++){
+
+            // Print stars
+            for (int k = 1; k <= 2 * i - 1; k++) {
                 System.out.print("*");
             }
-        System.out.println();
+
+            System.out.println();
         }
-        for(int i=n;i>=1;i--){
-            for(int j=1;j<=n-i;j++){
+
+        // Print the lower half of the diamond
+        // Start from n-1 to avoid printing the middle row twice
+        for (int i = n - 1; i >= 1; i--) {
+
+            // Print leading spaces
+            for (int j = 1; j <= n - i; j++) {
                 System.out.print(" ");
+            }
+
+            // Print stars
+            for (int k = 1; k <= 2 * i - 1; k++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
         }
-        for(int k=1;k<=2*i-1;k++){
-            System.out.print("*");
-        }
-        System.out.println();
+
+        sc.close();
     }
-            
-    
-
-sc.close();
 }
-}
-
