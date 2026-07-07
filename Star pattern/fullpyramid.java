dc2c7d21f8ef707f688e0251eac0ebc24b20fd76
Rupsha Das <drupsha81@gmail.com>
2026-07-07 23:17:15 +0530

    Rename class to FullPyramid and fix formatting
    Refactor full pyramid class and improve formatting

--- Star pattern/fullpyramid.java
+++ Star pattern/fullpyramid.java
@@ -1,18 +1,31 @@
 import java.util.Scanner;
-public class fullpyramid {
+
+public class FullPyramid {
+
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
+
+        // Read the number of rows
         int n = sc.nextInt();
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=n-i;j++){
+
+        // Loop through each row
+        for (int i = 1; i <= n; i++) {
+
+            // Print leading spaces
+            for (int j = 1; j <= n - i; j++) {
                 System.out.print(" ");
+            }
+
+            // Print stars
+            for (int k = 1; k <= 2 * i - 1; k++) {
+                System.out.print("*");
+            }
+
+            // Move to the next line
+            System.out.println();
         }
-        for(int k=1;k<=2*i-1;k++){
-            System.out.print("*");
-        }
-        System.out.println();
+
+        sc.close();
     }
-    sc.close();
-}
-    
 }
