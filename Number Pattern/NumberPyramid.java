29cd7f1a559cda6af657e67d96e0f677c51b1ee3
Rupsha Das <drupsha81@gmail.com>
2026-07-14 23:54:27 +0530

    Improve comments in NumberPyramid.java
    Refactor comments for clarity and consistency.

--- Number Pattern/NumberPyramid.java
+++ Number Pattern/NumberPyramid.java
@@ -1,23 +1,28 @@
 import java.util.Scanner;
 
 public class NumberPyramid {
+
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
+
+        // Read the number of rows
         int n = sc.nextInt();
 
-        // Rows
+        // Traverse each row
         for (int i = 1; i <= n; i++) {
 
-            // Print spaces
+            // Print leading spaces
             for (int j = 1; j <= n - i; j++) {
                 System.out.print(" ");
             }
 
-            // Print numbers
+            // Print the current row number i, repeated i times
             for (int j = 1; j <= i; j++) {
                 System.out.print(i + " ");
             }
 
+            // Move to the next row
             System.out.println();
         }
 
