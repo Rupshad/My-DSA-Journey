f0a7a029cca116ea9ea006ac6b1b8a45a1cd028a
Rupsha Das <drupsha81@gmail.com>
2026-07-18 23:53:26 +0530

    Refactor input prompt and simplify sum calculation

--- Fundamentals/Logical Patterns/SumSeriesLoop.java
+++ Fundamentals/Logical Patterns/SumSeriesLoop.java
@@ -1,19 +1,25 @@
 import java.util.Scanner;
 
 public class SumSeriesLoop {
+
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter value of n: ");
+
+        // Read the value of n
+        System.out.print("Enter the value of n: ");
         int n = sc.nextInt();
 
         int sum = 0;
 
+        // Add numbers from 1 to n
         for (int i = 1; i <= n; i++) {
-            sum = sum + i;
+            sum += i;
         }
-        
 
+        // Display the sum
         System.out.println("Sum = " + sum);
+
         sc.close();
     }
 }
