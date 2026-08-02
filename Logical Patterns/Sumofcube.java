88f68cab5f0a2b95f762a056d8830816cbfa1cc0
Rupsha Das <drupsha81@gmail.com>
2026-08-02 23:49:39 +0530

    Refactor Sumofcube class and improve formatting

--- Logical Patterns/Sumofcube.java
+++ Logical Patterns/Sumofcube.java
@@ -1,20 +1,27 @@
 import java.util.Scanner;
 
-public class Sumofcube {
+public class SumOfCube {
+
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter value of n: ");
+
+        // Read the value of n
+        System.out.print("Enter the value of n: ");
         int n = sc.nextInt();
 
+        // Variable to store the sum of cubes
         int sum = 0;
 
+        // Calculate the sum of cubes from 1 to n
         for (int i = 1; i <= n; i++) {
-            sum = sum + (i*i*i);
+            sum += i * i * i;
         }
-        
 
+        // Display the final sum
         System.out.println("Sum = " + sum);
+
+        // Close the Scanner object
         sc.close();
     }
 }
-
