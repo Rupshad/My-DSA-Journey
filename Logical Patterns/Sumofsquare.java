8c7bdde04fdb70ef8450fe1881f5a4b63aee8dda
Rupsha Das <drupsha81@gmail.com>
2026-08-02 23:50:24 +0530

    Refactor Sumofsquare class and improve formatting

--- Logical Patterns/Sumofsquare.java
+++ Logical Patterns/Sumofsquare.java
@@ -1,20 +1,27 @@
 import java.util.Scanner;
 
-public class Sumofsquare {
+public class SumOfSquare {
+
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter value of n: ");
+
+        // Read the value of n
+        System.out.print("Enter the value of n: ");
         int n = sc.nextInt();
 
+        // Variable to store the sum of squares
         int sum = 0;
 
+        // Calculate the sum of squares from 1 to n
         for (int i = 1; i <= n; i++) {
-            sum = sum + (i*i);
+            sum += i * i;
         }
-        
 
+        // Display the final sum
         System.out.println("Sum = " + sum);
+
+        // Close the Scanner object
         sc.close();
     }
 }
-
