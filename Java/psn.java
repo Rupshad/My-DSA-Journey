e806a7b40840c3f27793c3fee6750dbe19e9690f
Rupsha Das <drupsha81@gmail.com>
2026-08-12 23:53:12 +0530

    Improve formatting and structure in psn.java
    Refactor main method for better readability and structure.

--- Java/psn.java
+++ Java/psn.java
@@ -1,20 +1,25 @@
 import java.util.Scanner;
+
 public class psn {
-    public static void main(String[] args){
+    public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
+
+        // Take input from the user
         System.out.println("Enter the number:");
         int n = sc.nextInt();
-        if(n>0){
+
+        // Check whether the number is positive, negative, or zero
+        if (n > 0) {
             System.out.println("The number is positive");
-        }
-        else if(n<0){
+        } 
+        else if (n < 0) {
             System.out.println("The number is negative");
-        }
-        else{
+        } 
+        else {
             System.out.println("The number is zero");
         }
+
         sc.close();
     }
-
-    
 }
