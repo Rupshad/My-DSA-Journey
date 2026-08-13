226b0297de0a86f00a7305a94b82375ae4406050
Rupsha Das <drupsha81@gmail.com>
2026-08-13 21:31:32 +0530

    Refactor EvenOdd class main method structure

--- Java/EvenOdd.java
+++ Java/EvenOdd.java
@@ -1,19 +1,21 @@
 import java.util.Scanner;
-public class EvenOdd {
-    public static void main (String[] args){
-        Scanner sc= new Scanner(System.in);
-        System.out.print("Enter the number:");
 
+public class EvenOdd {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        // Take input from the user
+        System.out.print("Enter the number: ");
         int num = sc.nextInt();
+
+        // Check whether the number is even or odd
         if (num % 2 == 0) {
             System.out.println("Even");
-            }
-            else {
-                System.out.println("Odd");
-                }
-                sc.close();
+        } else {
+            System.out.println("Odd");
+        }
 
-
-
+        sc.close();
     }
 }
