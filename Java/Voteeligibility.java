c47e8633a135b779678829d57a3ae464b34d31e7
Rupsha Das <drupsha81@gmail.com>
2026-08-16 23:53:31 +0530

    Refactor Voteeligibility class for clarity and formatting

--- Java/Voteeligibility.java
+++ Java/Voteeligibility.java
@@ -1,21 +1,22 @@
 import java.util.Scanner;
-public class Voteeligibility {
-    public static void main (String[] args){
-        Scanner sc= new Scanner(System.in);
-        System.out.println("Enter your age");
-        int age= sc.nextInt();
-        if(age>=18){
-            System.out.println("You are eligible to vote");
 
+public class Voteeligibility {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        // Take age as input
+        System.out.print("Enter your age: ");
+        int age = sc.nextInt();
+
+        // Check voting eligibility
+        if (age >= 18) {
+            System.out.println("You are eligible to vote");
         }
-        else{
+        else {
             System.out.println("You are not eligible to vote");
         }
+
         sc.close();
-
     }
-
-
-
-    
 }
