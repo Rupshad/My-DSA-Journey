491ec71d3085d8aa31892e2d875f66e9b4c2ab02
Rupsha Das <drupsha81@gmail.com>
2026-08-23 23:46:33 +0530

    Refactor absoluteValue method and main method

--- Functions/AbsoluteValue.java
+++ Functions/AbsoluteValue.java
@@ -1,21 +1,31 @@
 import java.util.*;
+
 public class AbsoluteValue {
+
+    // Method to find the absolute value of a number
     public static int absoluteValue(int number) {
+
+        // If the number is negative, change its sign
         if (number < 0) {
             return -number;
         }
-        else{
+        else {
             return number;
         }
-        
     }
-    public static void main(String[] args){
-        Scanner sc= new Scanner(System.in);
-        System.out.println("Enter a number");
-        int number= sc.nextInt();
-        System.out.println("The absolute value of the number is "+absoluteValue(number));
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        // Take a number as input
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+
+        // Call the method and display the absolute value
+        System.out.println("The absolute value of the number is "
+                + absoluteValue(number));
+
         sc.close();
     }
 }
-    
-
