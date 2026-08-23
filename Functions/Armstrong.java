3f1568bd1d287f7507d05b9f3725fa79048d6068
Rupsha Das <drupsha81@gmail.com>
2026-08-23 23:47:16 +0530

    Refactor Armstrong number checking method

--- Functions/Armstrong.java
+++ Functions/Armstrong.java
@@ -1,37 +1,50 @@
 import java.util.*;
+
 public class Armstrong {
-    public static String isArmstrong(int num){
+
+    // Method to check whether a number is an Armstrong number
+    public static String isArmstrong(int num) {
+
         int original = num;
-        int count=0;
+        int count = 0;
         int temp = num;
-        while(temp>0){
-            temp = temp/10;
+
+        // Count the number of digits
+        while (temp > 0) {
+            temp = temp / 10;
             count++;
         }
-        int sum=0;
+
+        int sum = 0;
         temp = num;
-        while(temp>0){
-            int digit = temp%10;
-            sum = sum + (int)Math.pow(digit, count);
-            temp = temp/10;
+
+        // Calculate the sum of each digit raised to the number of digits
+        while (temp > 0) {
+            int digit = temp % 10;
+            sum = sum + (int) Math.pow(digit, count);
+            temp = temp / 10;
         }
+
+        // Compare the calculated sum with the original number
         if (sum == original) {
             return "Armstrong";
-        
-        } else {
-            return "Not Armstrong";}
         }
-        public static void main(String[] args) {
-            Scanner sc= new Scanner(System.in);
-             System.out.println("Enter a number");
-        int num = sc.nextInt();
-        System.out.println(isArmstrong(num));
-        sc.close();
-        
-        
-
+        else {
+            return "Not Armstrong";
+        }
     }
 
+    public static void main(String[] args) {
 
-}    
+        Scanner sc = new Scanner(System.in);
 
+        // Take a number as input
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        // Check and display the result
+        System.out.println(isArmstrong(num));
+
+        sc.close();
+    }
+}
