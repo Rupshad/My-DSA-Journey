f5c97a07fd11ffdafe579f9488774f0137b593e3
Rupsha Das <drupsha81@gmail.com>
2026-08-23 23:45:51 +0530

    Refactor printTable method and improve formatting

--- Functions/Table.java
+++ Functions/Table.java
@@ -1,24 +1,27 @@
-import java.util.*;
-public class Table {
-    public static void printTable(int num){
-        for(int i=1;i<=20;i++){
-            System.out.println(num+"*"+i+"="+ num*i);
-        }
+import java.util.Scanner;
 
+public class Table {
+
+    // Method to print the multiplication table
+    public static void printTable(int num) {
+
+        // Loop from 1 to 20
+        for (int i = 1; i <= 20; i++) {
+            System.out.println(num + " * " + i + " = " + (num * i));
+        }
     }
 
+    public static void main(String[] args) {
 
-    
+        Scanner sc = new Scanner(System.in);
 
-public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
+        // Take a number as input
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
 
-    
-    System.out.println("Enter a number: ");
-    int number = sc.nextInt();
-    printTable(number);
-    sc.close();
+        // Call the method to print the table
+        printTable(number);
+
+        sc.close();
+    }
 }
-}
-
-
