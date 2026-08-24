2f959a34cc1c631ea41444dc6569afe8c045b0d3
Rupsha Das <drupsha81@gmail.com>
2026-08-24 23:10:50 +0530

    Refactor factorial method and improve input handling

--- Functions/factorial.java
+++ Functions/factorial.java
@@ -1,34 +1,42 @@
-import java.util.*;
+import java.util.Scanner;
+
 public class factorial {
-    public static int fact(int n){
-        if(n < 0){
+
+    // Method to calculate the factorial of a number
+    public static int fact(int n) {
+
+        // Factorial is not defined for negative numbers
+        if (n < 0) {
             System.out.println("Invalid number");
             return -1;
-                }
-                else if(n == 0 || n == 1){
-                    return 1;
-                }
-                else{
-                    int fact=1;
-                    for(int i=n;i>=1;i--){
-                        fact = fact * i;
-                    }
-                    return fact;}
-                }
-                public static void main(String[] args){
-                    Scanner sc = new Scanner(System.in);
-                    System.out.println("Enter the number");
-                    int num = sc.nextInt();
-                    System.out.println("The factorial is:"+ (fact(num)));
+        }
 
-                    sc.close();
+        // Factorial of 0 and 1 is 1
+        else if (n == 0 || n == 1) {
+            return 1;
+        }
 
-                }
+        int fact = 1;
 
-    
+        // Calculate factorial using a loop
+        for (int i = n; i >= 1; i--) {
+            fact = fact * i;
+        }
 
-    
+        return fact;
+    }
 
-    
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        // Take a number as input
+        System.out.print("Enter the number: ");
+        int num = sc.nextInt();
+
+        // Calculate and display the factorial
+        System.out.println("The factorial is: " + fact(num));
+
+        sc.close();
+    }
 }
-
