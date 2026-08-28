0b4acb67162696da5bb0ba53a5c407ffd906fe42
Rupsha Das <drupsha81@gmail.com>
2026-08-28 23:55:55 +0530

    Refactor Fibonacci method and improve input prompts

--- Functions/Fibonacci.java
+++ Functions/Fibonacci.java
@@ -1,24 +1,33 @@
-import java.util.*;
+import java.util.Scanner;
+
 public class Fibonacci {
-    public static void fibonacci(int n){
-        int a=0, b=1;
-        for(int i=1 ; i<=n ; i++){
-            System.out.print(a+" ");
-            int next=a+b;
-            a=b;
-            b=next;
+
+    // Method to print the first n Fibonacci terms
+    public static void fibonacci(int n) {
+
+        int a = 0, b = 1;
+
+        // Generate Fibonacci sequence
+        for (int i = 1; i <= n; i++) {
+
+            System.out.print(a + " ");
+
+            int next = a + b;
+            a = b;
+            b = next;
         }
     }
+
     public static void main(String[] args) {
-        Scanner sc= new Scanner(System.in);
-        System.out.println("Enter the number of terms: ");
-        int n= sc.nextInt();
+
+        Scanner sc = new Scanner(System.in);
+
+        // Take the number of terms as input
+        System.out.print("Enter the number of terms: ");
+        int n = sc.nextInt();
 
         fibonacci(n);
+
         sc.close();
-
-        
     }
-
-    
 }
