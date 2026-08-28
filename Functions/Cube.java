8bc99484742ddffcc37f0706c5efe9561db3013d
Rupsha Das <drupsha81@gmail.com>
2026-08-28 23:55:18 +0530

    Refactor Cube class and update input prompt
    Refactor Cube class for improved readability and formatting.

--- Functions/Cube.java
+++ Functions/Cube.java
@@ -1,14 +1,23 @@
-import java.util.*;
+import java.util.Scanner;
+
 public class Cube {
-    public static int cube(int n){
-        return n*n*n;
+
+    // Method to calculate the cube of a number
+    public static int cube(int n) {
+        return n * n * n;
     }
-    public static void main(String[] args){
-        Scanner sc= new Scanner(System.in);
-        System.out.println("Enter a number");
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        // Take a number as input
+        System.out.print("Enter a number: ");
         int num = sc.nextInt();
-        System.out.println("The cube is "+cube(num));
+
+        // Calculate and display the cube
+        System.out.println("The cube is " + cube(num));
+
         sc.close();
     }
 }
-
