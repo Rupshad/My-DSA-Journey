c78c17c6710d135b67951834a8b228a022756571
Rupsha Das <drupsha81@gmail.com>
2026-08-29 23:02:21 +0530

    Refactor GCD method for clarity

--- Functions/GCD.java
+++ Functions/GCD.java
@@ -1,26 +1,34 @@
 import java.util.*;
+
 public class GCD {
+
+    // Method to find the GCD using the Euclidean Algorithm
     public static int gcd(int a, int b) {
-        while(b!=0){
-            int remainder = a%b;
-            a=b;
-            b=remainder;
+
+        // Continue until the remainder becomes 0
+        while (b != 0) {
+            int remainder = a % b;
+            a = b;
+            b = remainder;
         }
+
         return a;
     }
+
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
+
+        // Take two numbers as input
         System.out.println("Enter the first number: ");
         int a = sc.nextInt();
+
         System.out.println("Enter the second number: ");
         int b = sc.nextInt();
+
+        // Calculate and display the GCD
         System.out.println("GCD of " + a + " and " + b + " is: " + gcd(a, b));
+
         sc.close();
     }
 }
-            
-
-        
-    
-
-
