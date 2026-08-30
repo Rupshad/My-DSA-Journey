9061a5498a4073e8d3697009df1f0c5ddf73754f
Rupsha Das <drupsha81@gmail.com>
2026-08-30 23:24:38 +0530

    Improve palindrome method implementation
    Refactor palindrome method for clarity and consistency.

--- Functions/Palindrome.java
+++ Functions/Palindrome.java
@@ -1,30 +1,38 @@
 import java.util.*;
+
 public class Palindrome {
-    public static String palindrome(int temp){
-        int rev=0;
+
+    // Method to check whether a number is a palindrome
+    public static String palindrome(int temp) {
+        int rev = 0;
         int original = temp;
-        while(temp>0){
-        int pop =temp%10;
-        rev=rev*10+pop;
-        temp=temp/10;
+
+        // Reverse the number
+        while (temp > 0) {
+            int pop = temp % 10;
+            rev = rev * 10 + pop;
+            temp = temp / 10;
         }
-        if(original==rev){
-            return "palindrome";
-        }
-        else{
-            return "not palindrome";
+
+        // Compare the reversed number with the original
+        if (original == rev) {
+            return "Palindrome";
+        } else {
+            return "Not Palindrome";
         }
     }
+
     public static void main(String[] args) {
-        Scanner sc= new Scanner(System.in);
+
+        Scanner sc = new Scanner(System.in);
+
+        // Take a number as input
         System.out.println("Enter a number");
         int num = sc.nextInt();
-        System.out.println("the number is "+ palindrome(num));
+
+        // Check and display whether the number is a palindrome
+        System.out.println("The number is " + palindrome(num));
+
         sc.close();
     }
-
-
-    
-
-    
 }
