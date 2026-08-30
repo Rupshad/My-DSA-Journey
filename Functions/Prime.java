52fb37b3889397cc612227d919a1271b078d3519
Rupsha Das <drupsha81@gmail.com>
2026-08-30 23:25:25 +0530

    Refactor prime checking method and improve output

--- Functions/Prime.java
+++ Functions/Prime.java
@@ -1,25 +1,36 @@
 import java.util.*;
+
 public class Prime {
+
+    // Method to check whether a number is prime
     public static String isPrime(int n) {
+
+        // Numbers less than or equal to 1 are not prime
         if (n <= 1) {
-            return "notprime";
+            return "Not Prime";
         }
-        for(int i=2;i<=n-1;i++){
-            if(n%i==0){
-                return "notprime";
+
+        // Check if the number is divisible by any number from 2 to n-1
+        for (int i = 2; i <= n - 1; i++) {
+            if (n % i == 0) {
+                return "Not Prime";
+            }
         }
+
+        return "Prime";
     }
-    return "prime";
-}
-public static void main(String[] args){
-    Scanner sc= new Scanner(System.in);
-    System.out.println("Enter a number: ");
-    int n= sc.nextInt();
-    System.out.println("the number is "+isPrime(n));
-    sc.close();
-}
-}
-        
 
-    
+    public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
+
+        // Take a number as input
+        System.out.println("Enter a number: ");
+        int n = sc.nextInt();
+
+        // Check and display whether the number is prime
+        System.out.println("The number is " + isPrime(n));
+
+        sc.close();
+    }
+}
