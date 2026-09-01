26a093e689c742cf75fa41df41b4b6771e3e6a7a
Rupsha Das <drupsha81@gmail.com>
2026-09-01 23:57:14 +0530

    Refactor reverse method and main input handling

--- Functions/Reverse.java
+++ Functions/Reverse.java
@@ -1,23 +1,32 @@
 import java.util.*;
+
 public class Reverse {
+
+    // Method to reverse a number
     public static int reverse(int x) {
-        int rev=0;
-        while(x>0){
-            int pop=x%10;
-            rev=rev*10+pop;
-            x=x/10;
+        int rev = 0;
+
+        // Extract digits from right to left and build the reversed number
+        while (x > 0) {
+            int pop = x % 10;
+            rev = rev * 10 + pop;
+            x = x / 10;
         }
+
         return rev;
     }
+
     public static void main(String[] args) {
-        Scanner sc= new Scanner(System.in);
+
+        Scanner sc = new Scanner(System.in);
+
+        // Take a number as input
         System.out.println("Enter a number");
-        int n=sc.nextInt();
-        System.out.println("The reversed number is:"+ reverse(n));
+        int n = sc.nextInt();
+
+        // Reverse and display the number
+        System.out.println("The reversed number is: " + reverse(n));
+
         sc.close();
     }
 }
-
-
-    
-
