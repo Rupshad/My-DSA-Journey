776bcda2fec64cac23d6791ab9fad67b9ffd5cb3
Rupsha Das <drupsha81@gmail.com>
2026-09-01 23:57:52 +0530

    Rename Sqrt method to square and update usage

--- Functions/Square.java
+++ Functions/Square.java
@@ -1,13 +1,23 @@
 import java.util.*;
+
 public class Square {
-    public static int Sqrt(int n){
-        return n*n;
+
+    // Method to calculate the square of a number
+    public static int square(int n) {
+        return n * n;
     }
-    public static void main(String[] args){
-        Scanner sc= new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        // Take a number as input
         System.out.println("Enter a number");
         int num = sc.nextInt();
-        System.out.println("The square is "+Sqrt(num));
+
+        // Calculate and display the square
+        System.out.println("The square is " + square(num));
+
         sc.close();
     }
 }
