c93d2ffc801aedf4c60a9bfbe11cd4185fba822c
Rupsha Das <drupsha81@gmail.com>
2026-09-02 23:50:14 +0530

    Refactor Sumoftwo class and method names

--- Functions/Sumoftwo.java
+++ Functions/Sumoftwo.java
@@ -1,21 +1,29 @@
 import java.util.*;
-public class Sumoftwo {
-    public static int SumofTwoint(int a, int b) {
-        int sum= a+b;
-        return sum;
-        
-        
+
+public class SumOfTwo {
+
+    // Method to calculate the sum of two integers
+    public static int sumOfTwoInt(int a, int b) {
+        return a + b;
     }
-    public static void main(String[] args){
-        Scanner sc= new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        // Take two numbers as input
         System.out.println("Enter first number");
-        int a= sc.nextInt();
+        int a = sc.nextInt();
+
         System.out.println("Enter second number");
-        int b= sc.nextInt();
-        int sum= SumofTwoint(a,b);
-        System.out.println("Sum of two numbers is "+sum);
+        int b = sc.nextInt();
+
+        // Calculate the sum
+        int sum = sumOfTwoInt(a, b);
+
+        // Display the result
+        System.out.println("Sum of two numbers is " + sum);
+
         sc.close();
     }
 }
-    
-
